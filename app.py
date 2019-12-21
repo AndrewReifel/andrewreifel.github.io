3dadbbfdefec69725dfb3f4bb3bf23432e3d62c0
Andrew Reifel <areifel@me.com>
2019-12-21 12:38:06 -0500

    how to run comment

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     
-    
     r = requests.get('https://api.coinmarketcap.com/v1/ticker/ethereum/')
     
     for coin in r.json():
@@ -21,6 +20,10 @@ def index():
     price = float(price)
     price = round(price, 2)
 
+    #print ETH price to console
+    print(price)
+
     #render index page with price
     return render_template("index.html", price=price)
 
+#In order to run in terminal call python3 -m flask run (not sure why "-m" is needed now)
